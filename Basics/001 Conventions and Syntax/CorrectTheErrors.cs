1897b6ec054e4c7caf17aa2d53b5e891060958ef
Solange88 <143556773+Solange88@users.noreply.github.com>
2023-09-05 18:52:37 -0600

    correct errors

--- Basics/001 Conventions and Syntax/CorrectTheErrors.cs
+++ Basics/001 Conventions and Syntax/CorrectTheErrors.cs
@@ -1,6 +1,6 @@
 //Use this file to store your work to demonstrate your understanding of operators
 //Make sure your work is different from the instructors examples or your peers
-using Sytem;
+using System;
 
 public class CorrectTheErrors
 {
