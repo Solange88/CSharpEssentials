f1e927caf8a9e473b8e8ec2e8154be2163facf8f
Solange88 <143556773+Solange88@users.noreply.github.com>
2023-09-05 20:20:01 -0600

    variables and operators

--- Basics/001 Variables And Operators/Operators.cs
+++ Basics/001 Variables And Operators/Operators.cs
@@ -1,2 +1,26 @@
 //Use this file to store your work to demonstrate your understanding of operators
 //Make sure your work is different from the instructors examples or your peers
+using System; 
+namespace Welcome
+{
+    class Program 
+    static void Main (string args) 
+    {
+        Console.WriteLine ("Welcome Everyone");
+        Console.WriteLine ("a test");
+        WriteLine ("to practice") ;
+       const char myletter = 'S';
+        Console.WriteLine (myletter);
+        int mynumber = 1;
+        Console.WriteLine (" it is my" + (myletter) + (mynumber) "attempt");
+char olet ='o';
+char llet = 'l';
+char alet = 'a';
+char nlet = 'n';
+char glet = 'g';
+char elet = 'e'; 
+string name = myletter + olet + llet + alet + nlet + glet + elet; 
+Console.WriteLine (name + "is learning to code")
+    }
+    
+}
