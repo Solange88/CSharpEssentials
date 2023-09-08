3c429c3ea7132f1f22047744ef311c976ec92422
Solange88 <143556773+Solange88@users.noreply.github.com>
2023-09-08 13:18:24 -0600

    Update MyClass.cs with Unit1a challenge

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -7,3 +7,41 @@
 //Add another class with any name, add variables 
 //Using the new keyword create several objects
 //Console writeline the Objects variables
+using System;
+					
+public class Program
+{
+	public Hero heroOne;
+	public Hero heroTwo;
+	public Hero heroThree;
+	public Hero hero;
+	public void Main()
+	
+	{
+		heroOne = new Hero();
+		heroTwo = new Hero();
+		
+		
+		heroOne.health = 2;
+		heroOne.powerLevel = 5;
+		
+		heroTwo.health = 3;
+		heroTwo.powerLevel = 1;
+		
+		hero.health = 4;
+	
+		Console.WriteLine("Write your Name.");
+		string heroThree = Console.ReadLine();
+		Console.WriteLine(" Hello " + heroThree);
+		Console.WriteLine("Your health is" + hero.health);
+		Console.WriteLine("Your power level is" + heroOne.powerLevel);
+		
+		Console.WriteLine(heroTwo.health);
+		Console.WriteLine(heroTwo.powerLevel);
+	}
+}
+
+public class Hero {
+	public int health;
+	public int powerLevel;
+}
